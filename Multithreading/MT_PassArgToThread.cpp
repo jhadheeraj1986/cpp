f9bd50bc9dc185bee1b4d08fd3534ff74cd51aa4
Dheeraj Jha <jha.dheeraj1986@gmail.com>
2020-02-22 21:21:57 +0530

    Improved explaination and added one more function to show pass vy value and pass by ref

--- Multithreading/MT_PassArgToThread.cpp
+++ Multithreading/MT_PassArgToThread.cpp
@@ -7,12 +7,17 @@ int shared;
 mutex mutObj;
 
 int fun1( const string str){
-    cout << str << endl;
+    cout << std::this_thread::get_id()<<"fun1(): "<< str << endl;
 }
 
-int fun2(int &x){
-    cout << x << endl;
+int fun2(int x){
+    cout << std::this_thread::get_id()<< "fun2(): "<< x << endl;
     x = 10;
+}
+
+int fun3(int &x){
+    cout << std::this_thread::get_id()<< "fun3(): "<< x << endl;
+    x = 20;
 }
 
 
@@ -20,21 +25,21 @@ int main(){
 
     int x = 5;
     std::thread t1(fun1, "I am in Thread1.");
-    /*  You can not simpaly pass a referance of a variable to a thread 
-        this line will give compile time error. Uncomment to check*/
     //std::thread t2(fun2, (x));  
 
-    /*  in order to pass a ref to a thread, c++11 has std::ref()*/
-    std::thread t3(fun2, std::ref(x));
+    /*  You can not simply pass a referance of a variable to a thread 
+      in order to pass a ref to a thread, c++11 has std::ref()*/
+    std::thread t3(fun3, std::ref(x));
+    //std::thread t3(fun3, (&x));
 
-    int *p = new int();
+    int *p = new int(99);
     //below commented lines will give compile time error
     //std::thread t4(fun2, p);    
     //std::thread t4(fun2, *p);  
-    //std::thread t5(fun2, std::ref(*p));    
-    std::thread t6(fun2, std::ref(*p));    
+    //std::thread t5(fun2, std::ref(p));    
+    std::thread t6(fun3, std::ref(*p));    
 
-    cout << "main() "<<x << endl;
+    //std::this_thread::sleep_for(std::chrono::seconds(2));
     t1.join();
     //t2.join();
     t3.join();
@@ -42,6 +47,8 @@ int main(){
     //t5.join();
     t6.join();
 
+    cout << "main() x: "<<x << endl;
+    cout << "main() *p: "<<*p << endl;
 
     return 0;
 }
