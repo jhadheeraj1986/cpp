9f5c358eb53f077d7e4f90d3bc9fb939535ba42e
Dheeraj Jha <jha.dheeraj1986@gmail.com>
2020-04-25 20:31:00 +0530

    Adding liciense information

--- WebServer/Server.cpp
+++ WebServer/Server.cpp
@@ -1,3 +1,29 @@
+/*!
+ *  @file       Server.cpp
+ *  @Project    Http Web Server implementation in cpp.
+ *  @author     Dheeraj Jha
+ *  @date       25/04/2020
+ *  @license    MIT - (c) 2020 - Dheeraj Jha - jha.dheeraj1986@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE. 
+ */
+
 #include "stdafx.h"
 #include "handler.h"
 /************************************************/
@@ -12,7 +38,7 @@ std::unique_ptr<Handler> g_httpHandler;
 void on_initialize(const string_t& address){
 	
 	uri_builder uri(address);
-	uri.append_path(U("get"));
+	//uri.append_path(U("get"));
 
 	auto addr = uri.to_uri().to_string();
 	//Create a handler object here who will server all requests.
@@ -42,10 +68,6 @@ int main(int argc, char* argv[]){
 	address.append(port);
 	//Initialize your server
 	on_initialize(address);
-
-/* 	for(int i=0; i<100; i++){ 
-		std::cout<<"This is for test"<<std::endl;
-		sleep(1);} */
 
 	std::cout << "Press ENTER to exit." << std::endl;
 
--- WebServer/handler.cpp
+++ WebServer/handler.cpp
@@ -1,3 +1,29 @@
+/*!
+ *  @file       handler.cpp
+ *  @Project    Http Web Server implementation in cpp.
+ *  @author     Dheeraj Jha
+ *  @date       25/04/2020
+ *  @license    MIT - (c) 2020 - Dheeraj Jha - jha.dheeraj1986@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE. 
+ */
+
 #include "stdafx.h"
 #include "handler.h"
 
@@ -11,11 +37,11 @@ using namespace web::http::experimental::listener;
 Handler::Handler(utility::string_t url) : m_listener(url)
 {
     m_listener.support(methods::GET, std::bind(&Handler::handle_get, this, std::placeholders::_1));
+    m_listener.support(methods::PUT, std::bind(&Handler::handle_put, this, std::placeholders::_1));
+    m_listener.support(methods::POST, std::bind(&Handler::handle_post, this, std::placeholders::_1));
+    m_listener.support(methods::DEL, std::bind(&Handler::handle_delete, this, std::placeholders::_1));
 }
 
-//
-// A GET of the dealer resource produces a list of existing tables.
-//
 void Handler::handle_get(http_request message)
 {
     ucout <<"Handle get: "<< message.to_string() << endl;
